991a6704de77a7912485815831c06f25a28ee364
N1klasSchmidt <156589475+N1klasSchmidt@users.noreply.github.com>
2026-09-01 15:56:24 +0200

    Small changes for server

---
+++ scripts/velocyto.sh
@@ -0,0 +1,7 @@
+velocyto run \
+  -b filtered_feature_bc_matrix/barcodes.tsv.gz \
+  -o /path/to/output_folder \
+  gex_possorted_bam.bam \
+  /omics/groups/OE0132/internal/agreco/opt/mm10/refdata-cellranger-arc-mm10-2020-A-2.0.0/genes/genes.gtf.gz
+
+  
